ccc6de8e9a5711cd09b2e6d9033092305c5e8bd6
millbren <43661519+millbren@users.noreply.github.com>
2020-09-25 10:23:53 -0700

    Code Changes

--- WeedSeeker-Loom2-7-27-BetterTesting/WeedSeeker-Loom2-7-27-BetterTesting.ino
+++ WeedSeeker-Loom2-7-27-BetterTesting/WeedSeeker-Loom2-7-27-BetterTesting.ino
@@ -37,7 +37,7 @@ LoomFactory<
 
 LoomManager Loom{ &ModuleFactory };
 
-int relaySet = A5;
+//int relaySet = A5;
 int ledpin = A1;
 
 void setup() 
@@ -46,84 +46,87 @@ void setup()
 	Loom.begin_serial(true);
 	Loom.parse_config(json_config);
 	Loom.print_config();
-  setup_ttl();
-	LPrintln("\n ** Setup Complete ** ");
+//  setup_ttl();
+	 LPrintln("\n ** Setup Complete ** ");
  
-  pinMode(relaySet, OUTPUT);
+//  pinMode(relaySet, OUTPUT);
   pinMode(ledpin, OUTPUT);
   
-  digitalWrite(relaySet, LOW);
-//  digitalWrite(ledpin, LOW); 
+//  digitalWrite(relaySet, LOW);
+  digitalWrite(ledpin, HIGH);
+  delay(2000);
+  digitalWrite(ledpin, LOW); 
   
 }
 
 void loop() 
 {
 
-
 //// ****  Sense and Store  **** ////
-  
+//   LPrintln("1111111111111111");
 	Loom.measure();
   auto package = Loom.package();
 	Loom.display_data();
+//   LPrintln("222222222222");
   Loom.SDCARD().log("ALL.csv");
+//   LPrintln("333333333333");
 
   String time = package["timestamp"]["time"].as<const char*>();
   String seconds = time.substring(6);
   int sec = seconds.toInt();
- 
-  while(sec%2 != 0){
-    package = Loom.package(); 
-    time = package["timestamp"]["time"].as<const char*>();
-    seconds = time.substring(6);
-    sec = seconds.toInt();
-  }
-
+//   LPrintln("444444444444444444");
+//  while(sec%2 != 0){
+//    package = Loom.package(); 
+//    time = package["timestamp"]["time"].as<const char*>();
+//    seconds = time.substring(6);
+//    sec = seconds.toInt();
+//  }
+//   LPrintln("5555555555555555555");
 
 //// ****  NDVI Calculation  **** ////
-  Serial.println("+++++ START +++++");
+  LPrintln("+++++ START +++++");
   JsonObject as7265x = (package["contents"].as<JsonArray>())[1]["data"];
-  uint16_t a = as7265x["410nm"]; a = (int)a; Serial.println(a);
-  uint16_t b = as7265x["435nm"]; b = (int)b; Serial.println(b);
-  uint16_t c = as7265x["460nm"]; c = (int)c; Serial.println(c);
-  uint16_t d = as7265x["485nm"]; d = (int)d; Serial.println(d);
-  uint16_t e = as7265x["510nm"]; e = (int)e; Serial.println(e);
-  uint16_t f = as7265x["535nm"]; f = (int)f; Serial.println(f);
-  uint16_t g = as7265x["560nm"]; g = (int)g; Serial.println(g);
-  uint16_t h = as7265x["585nm"]; h = (int)h; Serial.println(h);
-  uint16_t i = as7265x["610nm"]; i = (int)i; Serial.println(i);
-  uint16_t j = as7265x["645nm"]; j = (int)j; Serial.println(j);
-  uint16_t k = as7265x["680nm"]; k = (int)k; Serial.println(k);
-  uint16_t l = as7265x["705nm"]; l = (int)l; Serial.println(l); 
-  uint16_t m = as7265x["730nm"]; m = (int)m; Serial.println(m);
-  uint16_t n = as7265x["760nm"]; n = (int)n; Serial.println(n);
-  uint16_t o = as7265x["810nm"]; o = (int)o; Serial.println(o);
-  uint16_t p = as7265x["860nm"]; p = (int)p; Serial.println(p);
-  uint16_t q = as7265x["900nm"]; q = (int)q; Serial.println(q);
-  uint16_t r = as7265x["940nm"]; r = (int)r; Serial.println(r);
+  uint16_t a = as7265x["410nm"]; a = (int)a;  LPrintln(a);
+  uint16_t b = as7265x["435nm"]; b = (int)b;  LPrintln(b);
+  uint16_t c = as7265x["460nm"]; c = (int)c;  LPrintln(c);
+  uint16_t d = as7265x["485nm"]; d = (int)d;  LPrintln(d);
+  uint16_t e = as7265x["510nm"]; e = (int)e;  LPrintln(e);
+  uint16_t f = as7265x["535nm"]; f = (int)f;  LPrintln(f);
+  uint16_t g = as7265x["560nm"]; g = (int)g;  LPrintln(g);
+  uint16_t h = as7265x["585nm"]; h = (int)h;  LPrintln(h);
+  uint16_t i = as7265x["610nm"]; i = (int)i;  LPrintln(i);
+  uint16_t j = as7265x["645nm"]; j = (int)j;  LPrintln(j);
+  uint16_t k = as7265x["680nm"]; k = (int)k;  LPrintln(k);
+  uint16_t l = as7265x["705nm"]; l = (int)l;  LPrintln(l); 
+  uint16_t m = as7265x["730nm"]; m = (int)m;  LPrintln(m);
+  uint16_t n = as7265x["760nm"]; n = (int)n;  LPrintln(n);
+  uint16_t o = as7265x["810nm"]; o = (int)o;  LPrintln(o);
+  uint16_t p = as7265x["860nm"]; p = (int)p;  LPrintln(p);
+  uint16_t q = as7265x["900nm"]; q = (int)q;  LPrintln(q);
+  uint16_t r = as7265x["940nm"]; r = (int)r;  LPrintln(r);
 
   float total = a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r;
   
-  Serial.println("++++++++++++++++");  
-  float a_w = a/total; Serial.println(a_w);
-  float b_w = b/total; Serial.println(b_w);
-  float c_w = c/total; Serial.println(c_w);
-  float d_w = d/total; Serial.println(d_w);
-  float e_w = b/total; Serial.println(e_w);
-  float f_w = b/total; Serial.println(f_w);
-  float j_w = j/total; Serial.println(j_w); 
-  float k_w = k/total; Serial.println(k_w);
-  float m_w = m/total; Serial.println(m_w);
-  float n_w = n/total; Serial.println(n_w);
-  float o_w = o/total; Serial.println(o_w);
+   LPrintln("++++++++++++++++");  
+  float a_w = a/total;  LPrintln(a_w);
+  float b_w = b/total;  LPrintln(b_w);
+  float c_w = c/total;  LPrintln(c_w);
+  float d_w = d/total;  LPrintln(d_w);
+  float e_w = b/total;  LPrintln(e_w);
+  float f_w = b/total;  LPrintln(f_w);
+  float j_w = j/total;  LPrintln(j_w); 
+  float k_w = k/total;  LPrintln(k_w);
+  float m_w = m/total;  LPrintln(m_w);
+  float n_w = n/total;  LPrintln(n_w);
+  float o_w = o/total;  LPrintln(o_w);
 
   float ndvib = (((m_w + n_w + j_w + k_w) - 2*(b_w + c_w))/((m_w + n_w + j_w + k_w) + 2*(b_w* + c_w)));
  
-  Serial.println("++++++++++++++++");
-  Serial.println(total);
-  Serial.println(b_w);
-  Serial.println(ndvib);
-//  Serial.println(sec);Serial.println(" Seconds+++"); 
+  LPrintln("++++++++++++++++");
+  LPrintln(total);
+//  LPrintln(b_w);
+  LPrintln(ndvib);
+//  LPrintln(sec);// LPrintln(" Seconds+++"); 
 
 
 //// ****  10-Sec Intervals  **** ////
@@ -141,11 +144,13 @@ void loop()
 //// ****  ALGORITHM  **** ////
 
   if (ndvib > 0.25) {
-     if ((a < c && b < c && d < c && e < c && f < c && k < o  && k < p)){ // testing ratios
-          Serial.println("++++++++++++++++++++++++++++++++++++++");
-          Serial.println("++++++++++ This Is A Target ++++++++++");
-          Serial.println("++++++++++++++++++++++++++++++++++++++");
+     if ((a < c && b < c && d < c && e < c && f < c && k < o && i < o)){ // testing ratios
+          LPrintln("++++++++++++++++++++++++++++++++++++++");
+          LPrintln("++++++++++ This Is A Target ++++++++++");
+          LPrintln("++++++++++++++++++++++++++++++++++++++");
+          digitalWrite(ledpin, HIGH);
     //    snapgreen();
+    
 //        if (total <= 200){
 //          if (ndvib > 0.35){
 //            digitalWrite(ledpin, HIGH);
@@ -161,46 +166,48 @@ void loop()
 //            Loom.SDCARD().log("Green.csv");        
 //          }
 //        }
-      unlatchedRelay(1);
+//      unlatchedRelay(1);
       Loom.SDCARD().log("Green.csv");
       delay(500);
-      unlatchedRelay(0);
+//      unlatchedRelay(0);
       digitalWrite(ledpin, LOW);
     }
     else {
   //    unlatchedRelay(0);
-      Serial.println("----------------------------------");
-      Serial.println("++++++++++ Not A Target ++++++++++");
-      Serial.println("----------------------------------");
+      digitalWrite(ledpin, LOW);
+      LPrintln("----------------------------------");
+      LPrintln("++++++++++ Not A Target ++++++++++");
+      LPrintln("----------------------------------");
     }
   }
   
   else {
 //    unlatchedRelay(0);
-    Serial.println("----------------------------------");
-    Serial.println("++++++++++ Not A Target ++++++++++");
-    Serial.println("----------------------------------");
+    digitalWrite(ledpin, LOW);
+    LPrintln("----------------------------------");
+    LPrintln("++++++++++ Not A Target ++++++++++");
+    LPrintln("----------------------------------");
   }
-  Serial.println("+++++ End +++++"); 
+  LPrintln("+++++ End +++++"); 
   Loom.pause();
 }
 
 // ================================================================ 
 // ===                        LATCHED RELAY                     ===
 // ================================================================  
-void unlatchedRelay(bool io)
-{
-  if(io)
-  {
-    digitalWrite(relaySet, HIGH);   // turn A5 on
-    delay(2500);
-    digitalWrite(relaySet, LOW);    //turn A5 off
-  }
-  else                       
-  {  
-    digitalWrite(relaySet, LOW);    //turn A5 off
-  }
-  
-  delay(10); // wait for stability
-  
-}
+//void unlatchedRelay(bool io)
+//{
+//  if(io)
+//  {
+//    digitalWrite(relaySet, HIGH);   // turn A5 on
+//    delay(2500);
+//    digitalWrite(relaySet, LOW);    //turn A5 off
+//  }
+//  else                       
+//  {  
+//    digitalWrite(relaySet, LOW);    //turn A5 off
+//  }
+//  
+//  delay(10); // wait for stability
+//  
+//}
