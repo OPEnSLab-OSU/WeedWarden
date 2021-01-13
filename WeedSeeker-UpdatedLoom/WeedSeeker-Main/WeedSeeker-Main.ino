96e12999a5fdcdb6e1202f330ca00bcf41c66590
Liam Duncan <duncanli@oregonstate.edu>
2021-01-13 11:04:15 -0800

    Added Pictures Folder

--- WeedSeeker-UpdatedLoom/WeedSeeker-Main/WeedSeeker-Main.ino
+++ WeedSeeker-UpdatedLoom/WeedSeeker-Main/WeedSeeker-Main.ino
@@ -28,8 +28,8 @@ const char* json_config =
 
 #define VBATPIN A7  // pin connected to the 3.7V battery 
 #define LVEn 5      // pin 5 is the 3.3V Enable
-#define HVEn 6      // pin 6 is the 5V and V+ Enable 
-#define Buzzer A2   // Set the buzzer pin as A2 
+//#define HVEn 6      // pin 6 is the 5V and V+ Enable 
+#define Buzzer 6   // Set the buzzer pin as A2 
 
 // Set enabled modules
 LoomFactory<
@@ -44,7 +44,7 @@ LoomManager Loom{ &ModuleFactory };
 
 // Make global variables for calibration variables
  float total_ndvi = 0;
- float  ndvi[15];
+ float  ndvi[16];
  float STD; 
  int sensitivity = 1;
 
@@ -54,10 +54,10 @@ void setup()
 { 
   
   pinMode(LVEn, OUTPUT);    // LVEn is the pin that enables the hypnos board 3.3V 
-  pinMode(HVEn, OUTPUT);    // Enable the 5V output
+//  pinMode(HVEn, OUTPUT);    // Enable the 5V output
   pinMode(Buzzer, OUTPUT); 
 
-  digitalWrite(HVEn, HIGH);  // Turn on the growlight
+//  digitalWrite(HVEn, HIGH);  // Turn on the growlight
   digitalWrite(LVEn, LOW);  // Turn on the 3.3V Rail  for I2C control and SD card logging
   digitalWrite(Buzzer, LOW); // Start with the buzzer off 
 
@@ -128,7 +128,7 @@ void calibrate()
   for(int i=0; i<16; i++)
     total_ndvi += ndvi[i];
   
-  total_ndvi=(total_ndvi/16);
+  total_ndvi=(total_ndvi/16)+.02;
 
   // loop to calculate Standard deviation 
   float STD_sigma = 0;
@@ -202,44 +202,44 @@ void loop()
     //// ****  NDVI Calculation  **** ////
     Serial.println("+++++ START +++++");
     JsonObject as7265x = (SensorData["contents"].as<JsonArray>())[1]["data"];
-    uint16_t a = as7265x["a"]; a = (float)a; Serial.println(a); // 410
-    uint16_t b = as7265x["b"]; b = (float)b; Serial.println(b); // 435
-    uint16_t c = as7265x["c"]; c = (float)c; Serial.println(c); // 460
-    uint16_t d = as7265x["d"]; d = (float)d; Serial.println(d); // 485
-    uint16_t e = as7265x["e"]; e = (float)e; Serial.println(e); // 510
-    uint16_t f = as7265x["f"]; f = (float)f; Serial.println(f); // 535
-    uint16_t g = as7265x["g"]; g = (float)g; Serial.println(g); // 560
-    uint16_t h = as7265x["h"]; h = (float)h; Serial.println(h); // 585
-    uint16_t i = as7265x["i"]; i = (float)i; Serial.println(i); // 645
-    uint16_t j = as7265x["j"]; j = (float)j; Serial.println(j); // 705
-    uint16_t k = as7265x["k"]; k = (float)k; Serial.println(k); // 900
-    uint16_t l = as7265x["l"]; l = (float)l; Serial.println(l); // 940
-    uint16_t r = as7265x["r"]; r = (float)r; Serial.println(r); // 610
-    uint16_t s = as7265x["s"]; s = (float)s; Serial.println(s); // 680
-    uint16_t t = as7265x["t"]; t = (float)t; Serial.println(t); // 730
-    uint16_t u = as7265x["u"]; u = (float)u; Serial.println(u); // 760
-    uint16_t v = as7265x["v"]; v = (float)v; Serial.println(v); // 810
-    uint16_t w = as7265x["w"]; w = (float)w; Serial.println(w); // 860
+    uint16_t a = as7265x["a"]; a = (float)a; //Serial.println(a); // 410
+    uint16_t b = as7265x["b"]; b = (float)b; //Serial.println(b); // 435
+    uint16_t c = as7265x["c"]; c = (float)c; //Serial.println(c); // 460
+    uint16_t d = as7265x["d"]; d = (float)d; //Serial.println(d); // 485
+    uint16_t e = as7265x["e"]; e = (float)e; //Serial.println(e); // 510
+    uint16_t f = as7265x["f"]; f = (float)f; //Serial.println(f); // 535
+    uint16_t g = as7265x["g"]; g = (float)g; //Serial.println(g); // 560
+    uint16_t h = as7265x["h"]; h = (float)h; //Serial.println(h); // 585
+    uint16_t i = as7265x["i"]; i = (float)i; //Serial.println(i); // 645
+    uint16_t j = as7265x["j"]; j = (float)j; //Serial.println(j); // 705
+    uint16_t k = as7265x["k"]; k = (float)k; //Serial.println(k); // 900
+    uint16_t l = as7265x["l"]; l = (float)l; //Serial.println(l); // 940
+    uint16_t r = as7265x["r"]; r = (float)r; //Serial.println(r); // 610
+    uint16_t s = as7265x["s"]; s = (float)s; //Serial.println(s); // 680
+    uint16_t t = as7265x["t"]; t = (float)t; //Serial.println(t); // 730
+    uint16_t u = as7265x["u"]; u = (float)u; //Serial.println(u); // 760
+    uint16_t v = as7265x["v"]; v = (float)v; //Serial.println(v); // 810
+    uint16_t w = as7265x["w"]; w = (float)w; //Serial.println(w); // 860
 
     float total = a + b + c + d + e + f + g + h + i + j + k + l + r + s + t + u + v + w;
     
     Serial.println("++++++++++++++++");
     Serial.println("++++++++++++++++");    
-    float a_w = a/total; Serial.println(a_w);
-    float b_w = b/total; Serial.println(b_w);
-    float c_w = c/total; Serial.println(c_w);
-    float d_w = d/total; Serial.println(d_w);
-    float e_w = b/total; Serial.println(e_w);
-    float f_w = b/total; Serial.println(f_w);
-    float g_w = g/total; Serial.println(g_w);
-    float j_w = j/total; Serial.println(j_w); 
-    float k_w = k/total; Serial.println(k_w);
-    float r_w = r/total; Serial.println(r_w);
-    float s_w = s/total; Serial.println(s_w);
-    float t_w = t/total; Serial.println(t_w);
-    float u_w = u/total; Serial.println(u_w);
-    float v_w = v/total; Serial.println(v_w);
-    float w_w = w/total; Serial.println(w_w);
+    float a_w = a/total; //Serial.println(a_w);
+    float b_w = b/total; //Serial.println(b_w);
+    float c_w = c/total; //Serial.println(c_w);
+    float d_w = d/total; //Serial.println(d_w);
+    float e_w = b/total; //Serial.println(e_w);
+    float f_w = b/total; //Serial.println(f_w);
+    float g_w = g/total; //Serial.println(g_w);
+    float j_w = j/total; //Serial.println(j_w); 
+    float k_w = k/total; //Serial.println(k_w);
+    float r_w = r/total; //Serial.println(r_w);
+    float s_w = s/total; //Serial.println(s_w);
+    float t_w = t/total; //Serial.println(t_w);
+    float u_w = u/total; //Serial.println(u_w);
+    float v_w = v/total; //Serial.println(v_w);
+    float w_w = w/total; //Serial.println(w_w);
 
     float nd = ((u + v + w + e + f + g) - 2*(b + c + d));
     float vib = ((u + v + w + e + f + g) + 2*(b + c + d));
