0917612391619d61c64debe76545900af39f6ba3
Liam Duncan <duncanli@oregonstate.edu>
2021-02-28 15:47:23 -0800

    Added more comments to the main arduino file

--- WeedSeeker-UpdatedLoom/WeedSeeker-Main/WeedSeeker-Main.ino
+++ WeedSeeker-UpdatedLoom/WeedSeeker-Main/WeedSeeker-Main.ino
@@ -1,35 +1,31 @@
 ///////////////////////////////////////////////////////////////////////////////
 
-// This is simple one of the simplest examples of using Loom. The code used
-// in this example shows up in the majority of other cases as well. 
+// FileName: WeedSeeker-Main.ino 
+// FileType: Arduino Source code file
+// Author: Liam Duncan
+// Organization: Oregon State Open Sensing Lab (OPEnS) 
+// Last Modified: 2/28/2021 
 
-// The include, config, LoomFactory, and LoomManager are typically used in
-// this way.
-
-// The setup almost always includes what is in this example, and in many 
-// cases, no further setup is necessary
-
-// The loop is also the start of what most cases use, namely: measure,
-// package, display, and wait (according to 'interval' in the config).
-// Other examples simply build off of this sequence
-
-// The configuration currently expects the relay is connected to pin 10
+// Description: 
+// This is the code for the OPEnS Lab Weed Warden. 
+// This code is meant to be uploaded to the feather M0 board and used to detect vegetation. 
+// Default setting for this code are to use the NDVIB algorithm and a threshold of .015 over the calibration value
 
 ///////////////////////////////////////////////////////////////////////////////
-// WeedWarden
+
+// Include the OPEnS Lab Loom library 
+// https://github.com/OPEnSLab-OSU/Loom/wiki/Quick-Start
 #include <Loom.h>
-//#include "Snapshot.h"
-//#include "Loom_AS7265X.cpp"
+
 
 // Include configuration
 const char* json_config = 
 #include "config.h"
 ;
 
-#define VBATPIN A7  // pin connected to the 3.7V battery 
-#define LVEn 5      // pin 5 is the 3.3V Enable
-//#define HVEn 6      // pin 6 is the 5V and V+ Enable 
-#define Buzzer 6   // Set the buzzer pin as A2 
+#define VBATPIN A7           // pin connected to the 3.7V battery 
+#define 3.3V_Rail_Enable 5   // pin 5 is the 3.3V Enable
+#define 12V_Rail_Enable 6    // pin 6 is the 5V and V+ Enable  
 
 // Set enabled modules
 LoomFactory<
@@ -43,23 +39,23 @@ LoomFactory<
 LoomManager Loom{ &ModuleFactory };
 
 // Make global variables for calibration variables
- float total_ndvi = 0;
- float  ndvi[16];
- float STD; 
- int sensitivity = 1;
+ float threshold = 0;   // variable to hold the calibration threshold
+ float ndvi[16];        // array to hold the NDVI values during calibration
+ float offset = .015;   // This value is the threshold offset 
 
 
 // Setup Function
 void setup() 
 { 
   
-  pinMode(LVEn, OUTPUT);    // LVEn is the pin that enables the hypnos board 3.3V 
-//  pinMode(HVEn, OUTPUT);    // Enable the 5V output
-  pinMode(Buzzer, OUTPUT); 
+  pinMode(3.3V_Rail_Enable, OUTPUT);    // 3.3V_Rail_Enable is the pin that enables the hypnos board 3.3V 
+  pinMode(12V_Rail_Enable, OUTPUT);     // 12V_Rail_Enable is the variable that will control the 5V and 12V Rails on the Hypnos board 
 
-//  digitalWrite(HVEn, HIGH);  // Turn on the growlight
-  digitalWrite(LVEn, LOW);  // Turn on the 3.3V Rail  for I2C control and SD card logging
-  digitalWrite(Buzzer, LOW); // Start with the buzzer off 
+  // 3.3V Enable is ACTIVE LOW.
+  digitalWrite(3.3V_Rail_Enable, LOW);  // Turn on the 3.3V Rail for I2C control and SD card logging
+
+  // 5V/12V rail is ACTIVE HIGH
+  digitalWrite(12V_Rail_Enable, LOW);   // Start with the 12V_Rail_Enable off 
 
   // Configure Loom and Serial 
 	Loom.begin_serial(true);
@@ -80,77 +76,60 @@ void setup()
 //=== NDVI Value that can be used as a threshold              ==== 
 //================================================================
 void calibrate()
-{
-    //DO THESE FIRST
-  //Get NDVIB lights on average for dirt to compare with grass
-  //plug into double if statement for NDVIB below
+{  
   for(int z=0; z<16; z++) // Loop to take calibration samples
   {
-    Loom.measure(); 
-      // Store data in JSON package
-    JsonObject calibrationData = Loom.package(); 
+    Loom.measure();   // Take data from the spectral triad sensor   
 
-    serializeJsonPretty(calibrationData, Serial);
+    JsonObject calibrationData = Loom.package();    // Store data in JSON package
 
-      //// ****  NDVI Calculation  **** ////
+    serializeJsonPretty(calibrationData, Serial);   // Serialize data 
+
+    // The following lines take the spectral data from the array and store them in 
+    // individual values. The wavelengths are listed on the side
     JsonObject as7265x = (calibrationData["contents"].as<JsonArray>())[1]["data"];
-    uint16_t a = as7265x["a"]; a = (int)a; Serial.println(a);
-    uint16_t b = as7265x["b"]; b = (int)b; Serial.println(b);
-    uint16_t c = as7265x["c"]; c = (int)c; Serial.println(c);
-    uint16_t d = as7265x["d"]; d = (int)d; Serial.println(d);
-    uint16_t e = as7265x["e"]; e = (int)e; Serial.println(e);
-    uint16_t f = as7265x["f"]; f = (int)f; Serial.println(f);
-    uint16_t g = as7265x["g"]; g = (int)g; Serial.println(g);
-    uint16_t h = as7265x["h"]; h = (int)h; Serial.println(h);
-    uint16_t i = as7265x["i"]; i = (int)i; Serial.println(i);
-    uint16_t j = as7265x["j"]; j = (int)j; Serial.println(j);
-    uint16_t k = as7265x["k"]; k = (int)k; Serial.println(k);
-    uint16_t l = as7265x["l"]; l = (int)l; Serial.println(l); 
-    uint16_t r = as7265x["r"]; r = (int)r; Serial.println(r);
-    uint16_t s = as7265x["s"]; s = (int)s; Serial.println(s);
-    uint16_t t = as7265x["t"]; t = (int)t; Serial.println(t);
-    uint16_t u = as7265x["u"]; u = (int)u; Serial.println(u);
-    uint16_t v = as7265x["v"]; v = (int)v; Serial.println(v);
-    uint16_t w = as7265x["w"]; w = (int)w; Serial.println(w);
+    uint16_t a = as7265x["a"]; a = (float)a; // 410
+    uint16_t b = as7265x["b"]; b = (float)b; // 435
+    uint16_t c = as7265x["c"]; c = (float)c; // 460
+    uint16_t d = as7265x["d"]; d = (float)d; // 485
+    uint16_t e = as7265x["e"]; e = (float)e; // 510
+    uint16_t f = as7265x["f"]; f = (float)f; // 535
+    uint16_t g = as7265x["g"]; g = (float)g; // 560
+    uint16_t h = as7265x["h"]; h = (float)h; // 585
+    uint16_t i = as7265x["i"]; i = (float)i; // 645
+    uint16_t j = as7265x["j"]; j = (float)j; // 705
+    uint16_t k = as7265x["k"]; k = (float)k; // 900
+    uint16_t l = as7265x["l"]; l = (float)l; // 940
+    uint16_t r = as7265x["r"]; r = (float)r; // 610
+    uint16_t s = as7265x["s"]; s = (float)s; // 680
+    uint16_t t = as7265x["t"]; t = (float)t; // 730
+    uint16_t u = as7265x["u"]; u = (float)u; // 760
+    uint16_t v = as7265x["v"]; v = (float)v; // 810
+    uint16_t w = as7265x["w"]; w = (float)w; // 860
 
 
    // calculate ndvi value 
+   // This is where the user would change the algorithm for calibration 
+
    float nd = ((u + v + w + e + f + g) - 2*(b + c + d));  // calculate top half of NDVI 
    float vib = ((u + v + w + e + f + g) + 2*(b + c + d)); // Calculate bottom half of NDVI
    ndvi[z] = nd/vib;  // Store NDVI values in an array
 
-   LPrintln(ndvi[z]); 
+   LPrintln(ndvi[z]);   // print out the calculated NDVI value
    
-   // delay(500);   // Put this delay back in if things get messed up 
   } // for loop 
 
-    // loop to average ndvi values 
+  // loop to sum up the ndvi values 
   for(int i=0; i<16; i++)
-    total_ndvi += ndvi[i];
+    threshold += ndvi[i];
   
-  total_ndvi=(total_ndvi/16)+.015;
+  threshold = (threshold / 16) + offset;  // divide by 16 to average values then add an offset to the value
 
-  // loop to calculate Standard deviation 
-  float STD_sigma = 0;
-  for(int i=0; i<16; i++)
-  {
-    STD_sigma += ((ndvi[i]-total_ndvi)*2);
-    if(i==15)
-    {
-      STD =(STD_sigma/16)*(1/2);
-    } // if
-  } // for 
-
-  // Print out the standard deviation 
-  LPrint("STD = "); 
-  LPrintln(STD); 
-
-  // Maybe add something in here that if the STD is too high we need to recalibrate 
-
-  // Turn on the buzzer for 6 seconds so that you know the calibration worked
-  digitalWrite(Buzzer, HIGH); 
+  // Turn on the 12V_Rail_Enable for 6 seconds so that you know the calibration worked
+  // In our tests we had the 5V rail hooked up to a 5V buzzer so that we knew when the calibration had finished
+  digitalWrite(12V_Rail_Enable, HIGH); 
   delay(6000);               
-  digitalWrite(Buzzer, LOW); 
+  digitalWrite(12V_Rail_Enable, LOW); 
 } // Calibrate 
 
 
@@ -158,7 +137,7 @@ void calibrate()
 //================================================================
 //===                     Measure battery                     ====
 //===                                                         ====
-//=== Function displays the battery voltage of a 3.7V battery ====
+//=== Function returns the battery voltage of a 3.7V battery ====
 //=== that is connected to the JST connector of the Feather   ==== 
 //================================================================
 int MeasureBat()
@@ -176,7 +155,6 @@ int MeasureBat()
 }
 
 
-
 // Main loop! 
 void loop()
 {
@@ -185,7 +163,6 @@ void loop()
   calibrate(); 
 
   while(1){
-    int LightState = 1;   // make the light state on 
 
     // Measure the data from sensor 
     Loom.measure();                         
@@ -199,7 +176,7 @@ void loop()
     // Serialize the JSON Object and print it to the Serial Monitor
     serializeJsonPretty(SensorData, Serial); 
 
-    //// ****  NDVI Calculation  **** ////
+    // put the spectral values into their own variables
     Serial.println("+++++ START +++++");
     JsonObject as7265x = (SensorData["contents"].as<JsonArray>())[1]["data"];
     uint16_t a = as7265x["a"]; a = (float)a; //Serial.println(a); // 410
@@ -221,8 +198,10 @@ void loop()
     uint16_t v = as7265x["v"]; v = (float)v; //Serial.println(v); // 810
     uint16_t w = as7265x["w"]; w = (float)w; //Serial.println(w); // 860
 
+    // Sum up the total of all wavelengths
     float total = a + b + c + d + e + f + g + h + i + j + k + l + r + s + t + u + v + w;
     
+    // calculate normalized wavelength values
     Serial.println("++++++++++++++++");
     Serial.println("++++++++++++++++");    
     float a_w = a/total; //Serial.println(a_w);
@@ -241,24 +220,35 @@ void loop()
     float v_w = v/total; //Serial.println(v_w);
     float w_w = w/total; //Serial.println(w_w);
 
+    // calculate the NDVIB Value
+    // This is where the user would need to change the code 
+    // in order to use a different detection algorithm
     float nd = ((u + v + w + e + f + g) - 2*(b + c + d));
     float vib = ((u + v + w + e + f + g) + 2*(b + c + d));
     float ndvib = nd/vib; 
 
+    // Calculate some other indices that are not used
+
+    // PSND
     float ps = v-f;
     float nd2 = v+f;
     float psnd = ps/nd2;
-    bool isgreen = 0; 
-  
+
+    // EVI   
     float evi = ((2.5*(v-s))/(v+6*s-7.5*b+1));
 
-    // Add Values to the JSON Package
+    // Add Values to the JSON Package so that they will be logged to SD Card
     Loom.add_data("NDVIB", "NDVIB", ndvib);
     Loom.add_data("EVI", "EVI", evi);
-    Loom.add_data("Threshold", "Threshold", total_ndvi);
+    Loom.add_data("Threshold", "Threshold", threshold);
 
+    // Calculate normalized NDVIB 
     float ndvib_w = ((u_w + v_w + w_w + e_w + f_w + g_w) - 2*(b_w + c_w + d_w))/((u_w + v_w + w_w + e_w + f_w + g_w) + 2*(b_w + c_w + d_w));
+    
+    // Calculate normalized psnd 
     float psnd_w = ((v_w-f_w)/(v_w+f_w));
+
+    // Calculate normalized EVI 
     float evi_w = ((2.5*(v_w-s_w))/(v_w+6*s_w-7.5*b_w+1));
 
     // Add Values to the JSON Package
@@ -269,30 +259,23 @@ void loop()
     // Log the data to SD Card 
     Loom.SDCARD().log(); 
 
+    // Print to the serial monitor
     Serial.println("++++++++++++++++");
     LPrint("Total = "); LPrintln(total);
     
-    // LPrint("b_w = ");   LPrintln(b_w);
     LPrint("ndvi = "); LPrintln(ndvib);
-    LPrint("total_ndvi = "); LPrintln(total_ndvi);  
-    // LPrint("evi = "); LPrintln(evi);
-    // LPrint("evi_w = "); LPrintln(evi_w);
-    // LPrint("psnd = "); LPrintln(psnd);
-    // LPrint("psnd_w = "); LPrintln(psnd_w);
-    // LPrint("Growlight State = "); LPrintln(LightState); 
+    LPrint("threshold = "); LPrintln(threshold);  
 
-    int success_tier = 0;
 
-    //add tiered sureness of green... more loops, each loop adding +1 to tier
-    if(total_ndvi < ndvib && sensitivity > 0)
+    // Compare the threshold value to the measured value to determine the presence of vegetation
+    if(threshold < ndvib)
     {
-      success_tier++;
 
         Serial.println("++++++++++++++++++++++++++++++++++++++");
         Serial.println("++++++++++ This Is A Target ++++++++++");
         Serial.println("++++++++++++++++++++++++++++++++++++++");
 
-        digitalWrite(Buzzer, HIGH);   // Turn on the Buzzer if grass is detected
+        digitalWrite(12V_Rail_Enable, HIGH);   // Turn on the 12V_Rail_Enable if grass is detected
 
     } // if
     else {
@@ -300,7 +283,7 @@ void loop()
       Serial.println("++++++++++ Not A Target ++++++++++");
       Serial.println("----------------------------------");
 
-      digitalWrite(Buzzer, LOW);  // Turn the buzzer off if dirt is detected
+      digitalWrite(12V_Rail_Enable, LOW);  // Turn the 12V_Rail_Enable off if dirt is detected
     } // else
 
     Serial.println("+++++ End +++++"); 
